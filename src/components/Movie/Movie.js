a6d8ac4ddb4426c1a58624987c95e1b3c1caa2ae
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-02 13:37:31 +0600

    added movie overview transform, transition css effect

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -8,9 +8,13 @@ const Movie = (props) => {
     return (
         <div className='movie'>
               <img className='movie_img' src={IMG_API + poster_path} alt={title}/>
-              <div className="movie-info">
+              <div className="movie_info">
                   <h3>{title}</h3>
                   <span>{vote_average}</span>
+              </div>
+              <div className="movie_overview">
+                  <h2>Overview:</h2>
+                  <p>{overview}</p>
               </div>
         </div>
     );
