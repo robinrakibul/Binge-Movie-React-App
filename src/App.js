b0afbc4042460971d97cf732a2a6eb0fa825e471
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-02 13:04:29 +0600

    destructured title,poster_path, info etc and fetched to movie component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Movie from './components/Movie/Movie';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a7a9f509d8c20ce68bfea9beb5c502f3&page=1";
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=a7a9f509d8c20ce68bfea9beb5c502f3&query=";
 
@@ -19,7 +18,7 @@ function App() {
     <div className="App">
       <p>Hello App Js</p>
       {
-        movies.length>0 && movies.map(movie => (<Movie key={movie.id} data={movie}></Movie>))
+        movies.length>0 && movies.map(movie => (<Movie key={movie.id} movie={movie}></Movie>))
       }
     </div>
   );
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
 const Movie = (props) => {
+   const {title, poster_path, overview, vote_average} = props.movie;
     return (
         <div className='movie'>
-          {
-              props.movies
-          }
+              <img src={IMG_API + poster_path} alt={title}/>
+              <div className="movie-info">
+                  <h3>{title}</h3>
+                  <span>{vote_average}</span>
+              </div>
         </div>
     );
 };
