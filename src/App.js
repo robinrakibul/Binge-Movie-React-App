15026dacfd9fcdd71381b105917b8a22db3a4b17
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-02 12:56:32 +0600

    api fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Movie from './components/Movie/Movie';
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a7a9f509d8c20ce68bfea9beb5c502f3&page=1";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=a7a9f509d8c20ce68bfea9beb5c502f3&query=";
+
 function App() {
-  const movies = ['1','2','3'];
+  const [movies,setMovies] = useState([]);
+  useEffect(()=>{
+    fetch(FEATURED_API)
+    .then(res=> res.json())
+    .then(data =>setMovies(data.results))
+  },[])
   return (
     <div className="App">
       <p>Hello App Js</p>
       {
-        movies.map(movie =>(<Movie movies={movies}></Movie>))
+        movies.length>0 && movies.map(movie => (<Movie key={movie.id} data={movie}></Movie>))
       }
     </div>
   );
