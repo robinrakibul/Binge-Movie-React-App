21b65980b02a3028e3c0f74ae2efafa97ff2e276
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-03 12:34:07 +0600

    added search function, returning objects in search box error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Movie from './components/Movie/Movie';
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a7a9f509d8c20ce68bfea9beb5c502f3&page=1";
@@ -10,14 +9,36 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=a7a9f509d
 
 function App() {
   const [movies,setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   useEffect(()=>{
     fetch(FEATURED_API)
     .then(res=> res.json())
     .then(data =>setMovies(data.results))
-  },[])
+  },[]);
+   
+
+const handleOnSubmit = (e) =>{
+  e.preventDefault();
+
+  if(searchTerm){
+    fetch(SEARCH_API + searchTerm)
+    .then(res=> res.json())
+    .then(data =>setSearchTerm(data.results));
+    setSearchTerm('');
+  }
+  
+}
+
+const handleOnChange = (e) =>{
+  setSearchTerm(e.target.value);
+}
   return (
     <div>
-      <Header></Header>
+      <header>
+            <form onSubmit={handleOnSubmit}>
+            <input className='search' type="search" value={searchTerm} onChange={handleOnChange} placeholder='Search...' />
+            </form>
+        </header>
       <div className="movie_container">
         {
           movies.length>0 && movies.map(movie => (<Movie key={movie.id} movie={movie}></Movie>))
